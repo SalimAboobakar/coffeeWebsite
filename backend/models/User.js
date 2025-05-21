44a4cff9d8697b07be96313250ef246cfaa9b412
Salim <salim.a.alhafidh@gmail.com>
2025-05-21 13:07:54 +0400

    fix some error

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,14 +17,14 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Hash password before saving
+// قبل الحفظ، شفر الباسورد
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Compare plaintext password with hashed
+// قارن الباسورد النصّي مع المشفّر
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
