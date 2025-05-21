4c3f08a64e1ef5a5fa2c660154fdfb0bb2815d71
Salim <salim.a.alhafidh@gmail.com>
2025-05-21 13:11:51 +0400

    fix some error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
+// backend/config/db.js
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected");
+    console.log("✅ MongoDB connected");
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.error("❌ MongoDB connection error:", err);
     process.exit(1);
   }
 };
